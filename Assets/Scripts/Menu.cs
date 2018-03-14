d10e9a6796e6d568ce5951a7f102fe6d17b40d38
MirroredFate <Karl_daniel@hotmail.de>
2018-03-14 15:01:45 +0100

    MenuScene working

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Apartment");
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
 }
