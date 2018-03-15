a6569cce4e43dc6e9c5d94e94611d83387ee1d08
Viktoria <v.lierhaus@gmx.de>
2018-03-15 09:57:52 +0100

    idk

---
+++ Assets/Scripts/Speech/SpeechBehaviour.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpeechBehaviour : MonoBehaviour {
+
+
+	void Update ()
+    {
+       
+         if (Input.GetKeyDown(KeyCode.Space))
+         {
+             SceneManager.LoadScene("Apartment");
+         } 
+    }
+}
