ccdb7f737591bc07c503c40f3b2e4d6fe4867038
MirroredFate <Karl_daniel@hotmail.de>
2018-03-15 14:25:45 +0100

    interaction fix

--- Assets/Scripts/Conditions/Interaction.cs
+++ Assets/Scripts/Conditions/Interaction.cs
@@ -7,7 +7,7 @@ public class Interaction : MonoBehaviour {
     public bool talk;
     public bool convinced = false;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
