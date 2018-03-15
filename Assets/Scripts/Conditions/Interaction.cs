dff3201bb18f8e794e602e0f446f99809d037055
Viktoria <v.lierhaus@gmx.de>
2018-03-15 11:38:08 +0100

    interaction

---
+++ Assets/Scripts/Conditions/Interaction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class InteractionCondition : MonoBehaviour
+{
+    
+    public void OnTriggerEnter()
+    {
+        Talk();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<DialogTest>() != null)
+        {
+            Talk();
+        }
+    }
+
+
+
+}
