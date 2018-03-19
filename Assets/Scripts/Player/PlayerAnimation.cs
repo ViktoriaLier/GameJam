a2c67ad8c8f9fade6cbf2b2e77a99f63b01d4f87
ViktoriaLier <34091337+ViktoriaLier@users.noreply.github.com>
2018-03-19 23:04:23 +0100

    anim start

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    public Animator anim;
+    public Rigidbody rbody;
+
+    private float inputH;
+    private float inputV;
+    
+
+    void Star()
+    {
+        anim = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            anim.Play("Walk", -1, 1f);
+        }
+
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+
+        float moveX = inputH * 20f * Time.deltaTime;
+        float moveZ = inputV * 50f * Time.deltaTime;
+
+        rbody.velocity = new Vector3(moveX, 0f, moveZ);
+    }
+}
